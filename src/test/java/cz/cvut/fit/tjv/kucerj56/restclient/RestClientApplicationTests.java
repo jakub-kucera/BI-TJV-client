0332b6775ab55b5d8f39a0b00cabf343f3662b95
Jakub Kučera <kucerj56@fit.cvut.cz>
2020-12-12 22:53:57 +0100

    Inital commit(with project)

---
+++ src/test/java/cz/cvut/fit/tjv/kucerj56/restclient/RestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.kucerj56.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
