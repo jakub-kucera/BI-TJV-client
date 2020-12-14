2b54a4ea9a9878a307d1ee1b9322e3f5051ff118
Jakub Kučera <kucerj56@fit.cvut.cz>
2020-12-14 11:44:30 +0100

    Added Dormitory Resource

--- src/main/java/cz/cvut/fit/tjv/kucerj56/restclient/RestClientApplication.java
+++ src/main/java/cz/cvut/fit/tjv/kucerj56/restclient/RestClientApplication.java
@@ -1,13 +1,36 @@
 package cz.cvut.fit.tjv.kucerj56.restclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.web.client.RestTemplate;
+import resource.DormitoryResource;
 
 @SpringBootApplication
-public class RestClientApplication {
+//@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+public class RestClientApplication implements ApplicationRunner {
+    @Autowired
+    private DormitoryResource dormitoryResource;
 
-    public static void main(String[] args) {
-        SpringApplication.run(RestClientApplication.class, args);
+//    @Autowired
+//    public RestClientApplication(DormitoryResource dormitoryResource) {
+//        this.dormitoryResource = dormitoryResource;
+//    }
+
+    public static void main(String[] args) { SpringApplication.run(RestClientApplication.class, args);}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(dormitoryResource.readAll());
+    }
 }
---
+++ src/main/java/resource/DormitoryResource.java
@@ -0,0 +1,35 @@
+package resource;
+
+import cz.cvut.fit.tjv.kucerj56.restclient.dto.DormitoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DormitoryResource {
+    private final RestTemplate restTemplate;
+
+//    @Autowired
+    public DormitoryResource(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+//    public DormitoryResource(RestTemplateBuilder restTemplateBuilder) {
+//        restTemplate = restTemplateBuilder.rootUri(ROOT_RESOURCE_URL).build();
+//    }
+
+    private static final String ROOT_RESOURCE_URL = "http://localhost:8080/api/dormitories";
+    private static final String ONE_URI = "/{id}";
+
+    public DormitoryDTO readById(String id) {
+        return restTemplate.getForObject(ROOT_RESOURCE_URL + ONE_URI, DormitoryDTO.class, id);
+    }
+
+    public List<DormitoryDTO> readAll() {
+        return Arrays.asList(restTemplate.getForObject(ROOT_RESOURCE_URL, DormitoryDTO[].class).clone());
+    }
+}
